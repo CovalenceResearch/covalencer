4d4ee984e0719a1cb1778b4ef2f71cee1a94a288
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-09 15:58:33 +0200

    Add documentation

--- R/theme-covalence.R
+++ R/theme-covalence.R
@@ -1,16 +1,40 @@
+#' The default Covalence [ggplot2] theme
+#'
+#' @description
+#' This theme provides the defaults required to generate plots, using [ggplot2],
+#' that are aligned with the Covalence Research house style. The theme can be
+#' modified further by using `theme()`. Also consider using the various color
+#' palettes to ensure the house style is met.
+#'
+#' @param base_size    Base font size, in points. Default is 12.
+#' @param base_family  Base font family. Default is Lato.
+#'
+#' @return A ggplot2 [theme][ggplot2::theme] object.
+#' @export
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(data = msleep, aes(x = brainwt, y = bodywt)) +
+#'    geom_point() +
+#'    theme_covalence()
+#'
+#' ggplot(data = msleep, aes(x = awake, y = name)) +
+#'    geom_col() +
+#'    facet_wrap(vars(vore)) +
+#'    theme_covalence()
 theme_covalence <- function(base_size = 12,
-                            base_family = "Lato",
-                            struct_color = "#f0f0f0",
-                            text_color = "#000000") {
-    ggplot2::theme_bw(base_size = base_size,
+                            base_family = "sans") {
+
+    ggplot2::theme_light(base_size = base_size,
                       base_family = base_family) +
         ggplot2::theme(
-            axis.ticks = ggplot2::element_line(color = struct_color,
+            axis.ticks = ggplot2::element_line(color = "#FAFAFA",
                                                linewidth = ggplot2::rel(0.5)),
             axis.ticks.length = ggplot2::unit(2, "pt"),
             axis.title = ggtext::element_markdown(face = "bold",
                                                   size = ggplot2::rel(1.0)),
-            axis.text = ggtext::element_markdown(color = text_color,
+            axis.text = ggtext::element_markdown(color = "#000000",
                                                  size = ggplot2::rel(0.80)),
             legend.background = ggplot2::element_blank(),
             legend.direction = "horizontal",
@@ -20,10 +44,8 @@ theme_covalence <- function(base_size = 12,
             legend.title = ggtext::element_markdown(face = "bold",
                                                     size = ggplot2::rel(1.1)),
             legend.margin = ggplot2::margin(0, 0, 0, 0, "pt"),
-            panel.border = ggplot2::element_rect(color = struct_color),
-            panel.grid.minor = ggplot2::element_line(),
-            panel.grid.major = ggplot2::element_line(color = struct_color,
-                                                     linewidth = ggplot2::rel(0.5)),
+            panel.background = ggplot2::element_rect(fill = "#FFFFFF"),
+            panel.grid.minor = ggplot2::element_blank(),
             panel.spacing = ggplot2::unit(1, "lines"),
             plot.caption = ggtext::element_markdown(
                 margin = ggplot2::margin(4, 0, 0, 0, "pt"),
@@ -38,12 +60,12 @@ theme_covalence <- function(base_size = 12,
             plot.title = ggtext::element_markdown(face = "bold",
                                                   size = ggplot2::rel(1.5)),
             plot.title.position = "plot",
-            strip.background = ggplot2::element_rect(fill = struct_color),
+            strip.background = ggplot2::element_blank(),
             strip.text = ggtext::element_markdown(
-                color = text_color,
-                face = "italic",
+                color = "#326aa0",
+                face = "bold",
                 hjust = 0,
-                size = ggplot2::rel(1.1)
+                size = ggplot2::rel(1.0)
             )
         )
 }
