2541aeb38c0bd50fa684e3a2c616f2ea063ff0e6
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-10 08:59:19 +0200

    Use Covalence dark blue as strip background

--- R/theme-covalence.R
+++ R/theme-covalence.R
@@ -7,7 +7,7 @@
 #' palettes to ensure the house style is met.
 #'
 #' @param base_size    Base font size, in points. Default is 12.
-#' @param base_family  Base font family. Default is Lato.
+#' @param base_family  Base font family. Default is "sans".
 #'
 #' @return A ggplot2 [theme][ggplot2::theme] object.
 #' @export
@@ -25,12 +25,13 @@
 #'    theme_covalence()
 theme_covalence <- function(base_size = 12,
                             base_family = "sans") {
-
     ggplot2::theme_light(base_size = base_size,
-                      base_family = base_family) +
+                         base_family = base_family) +
         ggplot2::theme(
-            axis.ticks = ggplot2::element_line(color = "#FAFAFA",
-                                               linewidth = ggplot2::rel(0.5)),
+            axis.ticks = ggplot2::element_line(
+                color = "#FAFAFA",
+                linewidth = ggplot2::rel(0.5)
+            ),
             axis.ticks.length = ggplot2::unit(2, "pt"),
             axis.title = ggtext::element_markdown(face = "bold",
                                                   size = ggplot2::rel(1.0)),
@@ -39,11 +40,11 @@ theme_covalence <- function(base_size = 12,
             legend.background = ggplot2::element_blank(),
             legend.direction = "horizontal",
             legend.justification = "right",
+            legend.margin = ggplot2::margin(0, 0, 0, 0, "pt"),
             legend.position = "top",
             legend.text = ggtext::element_markdown(size = ggplot2::rel(1.0)),
             legend.title = ggtext::element_markdown(face = "bold",
                                                     size = ggplot2::rel(1.1)),
-            legend.margin = ggplot2::margin(0, 0, 0, 0, "pt"),
             panel.background = ggplot2::element_rect(fill = "#FFFFFF"),
             panel.grid.minor = ggplot2::element_blank(),
             panel.spacing = ggplot2::unit(1, "lines"),
@@ -60,9 +61,9 @@ theme_covalence <- function(base_size = 12,
             plot.title = ggtext::element_markdown(face = "bold",
                                                   size = ggplot2::rel(1.5)),
             plot.title.position = "plot",
-            strip.background = ggplot2::element_blank(),
+            strip.background = ggplot2::element_rect(fill = "#326aa0"),
             strip.text = ggtext::element_markdown(
-                color = "#326aa0",
+                color = "#FFFFFF",
                 face = "bold",
                 hjust = 0,
                 size = ggplot2::rel(1.0)
