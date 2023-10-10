0c3f36008fd2c801bda6a1a7b2ecaf8cfe80c970
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-10 12:58:55 +0200

    Add Covalence palette function

--- R/palettes-covalence.R
+++ R/palettes-covalence.R
@@ -8,7 +8,6 @@
 #' @details
 #' This function is taken from a 2022 [blog post](https://meghan.rbind.io/blog/2022-10-11-creating-custom-color-palettes-with-ggplot2/#defining-custom-colors-and-palettes) by Meghan Hall.
 #'
-#'
 #' @param ... Access specific colors
 #'
 #' @return A named vector of brand colors
@@ -32,7 +31,44 @@ covalence_colors <- function(...) {
     dots <- c(...)
 
     if (is.null(dots))
-    { return(covalence_cols) }
+    {
+        return(covalence_cols)
+    }
 
     covalence_cols[dots]
 }
+
+#' Covalence palette
+#'
+#' @description
+#' The various Covalence palettes:
+#'   * The _complete_ palette that contains all brand colors.
+#'   * The _main_ palette that contains the main, mostly blue-ish, brand colors.
+#'   * The _accent_ palette that contains additional, more prominent colors.
+#'
+#' @details
+#' This function is slightly modified from a 2022 [blog post](https://meghan.rbind.io/blog/2022-10-11-creating-custom-color-palettes-with-ggplot2/#defining-custom-colors-and-palettes) by Meghan Hall.
+#'
+#' @param palette Palette to use. One of 'complete', 'main', or 'accent'.
+#' @param ... Dots.
+#'
+#' @return A named vector of brand colors.
+#' @export
+#'
+#' @examples
+#' covalence_palette("main")
+covalence_palette <-
+    function(palette = c("complete", "main", "accent"),
+             ...) {
+        # Check arguments
+        rlang::arg_match(palette, values = c("complete", "main", "accent"))
+
+        # Palette
+        covalence_pals <- list(
+            `complete` = covalence_colors(),
+            `main` = covalence_colors("lightblue", "darkblue", "teal", "gray"),
+            `accent` = covalence_colors("red", "green", "orange", "purple")
+        )
+
+        covalence_pals[[palette]]
+    }
