225a46ed4df56c9da8b45ea84d04c0dfca5c8041
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-13 17:52:59 +0200

    Plot all quadrants (i.e, create a symmetric plot)

--- R/plot-psa.R
+++ R/plot-psa.R
@@ -7,17 +7,21 @@ plot_psa_scatter <- function(data,
                              point_shape = 16,
                              point_size = 2,
                              ...) {
+
+    # Calculate symmetrical plot limits
+    col_qalys <- rlang::as_string(rlang::ensym(delta_qalys))
+    col_costs <- rlang::as_string(rlang::ensym(delta_costs))
+
+    max_qalys <- max(abs(data[[col_qalys]]))
+    max_costs <- max(abs(data[[col_costs]]))
+
+    limits_qalys <- c(-max_qalys, max_qalys)
+    limits_costs <- c(-max_costs, max_costs)
+
+    # Create scatter plot
     p <- ggplot2::ggplot(data = data,
-                         mapping = ggplot2::aes(x = {
-                             {
-                                 delta_qalys
-                             }
-                         },
-                         y = {
-                             {
-                                 delta_costs
-                             }
-                         })) +
+                         mapping = ggplot2::aes(x = {{ delta_qalys }},
+                                                y = {{ delta_costs }})) +
         ggplot2::geom_hline(
             yintercept = 0,
             color = "#000000",
@@ -36,10 +40,13 @@ plot_psa_scatter <- function(data,
         ) +
         ggplot2::scale_x_continuous(
             "Incremental quality-adjusted life-years",
-            labels = scales::number_format(big.mark = ",")
+            labels = scales::number_format(big.mark = ","),
+            limits = limits_qalys
         ) +
-        ggplot2::scale_y_continuous("Incremenctal costs",
-                                    labels = scales::label_dollar(prefix = currency, big.mark = ",")) +
+        ggplot2::scale_y_continuous(
+            "Incremenctal costs",
+            labels = scales::label_dollar(prefix = currency, big.mark = ","),
+            limits = limits_costs) +
         theme_covalence() %+replace%
         theme(panel.grid.major = ggplot2::element_blank())
 
