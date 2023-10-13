6c2af70dc7961e5e013d002915d72a255536a967
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-13 17:12:07 +0200

    Divide plot into quadrants

--- R/plot-psa.R
+++ R/plot-psa.R
@@ -18,6 +18,16 @@ plot_psa_scatter <- function(data,
                                  delta_costs
                              }
                          })) +
+        ggplot2::geom_hline(
+            yintercept = 0,
+            color = "#000000",
+            linewidth = 0.5
+        ) +
+        ggplot2::geom_vline(
+            xintercept = 0,
+            color = "#000000",
+            linewidth = 0.5
+        ) +
         ggplot2::geom_jitter(
             alpha = point_alpha,
             color = point_color,
@@ -26,11 +36,10 @@ plot_psa_scatter <- function(data,
         ) +
         ggplot2::scale_x_continuous(
             "Incremental quality-adjusted life-years",
-            labels = scales::number_format(big.mark = ",")) +
-        ggplot2::scale_y_continuous(
-            "Incremenctal costs",
-            labels = scales::label_dollar(prefix = currency, big.mark = ",")
+            labels = scales::number_format(big.mark = ",")
         ) +
+        ggplot2::scale_y_continuous("Incremenctal costs",
+                                    labels = scales::label_dollar(prefix = currency, big.mark = ",")) +
         theme_covalence() %+replace%
         theme(panel.grid.major = ggplot2::element_blank())
 
