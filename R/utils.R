36be84a8998a0c887a28328889ca3ca4d22c238e
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-15 20:13:56 +0200

    Add utility function to check if string is a color

--- R/utils.R
+++ R/utils.R
@@ -8,4 +8,19 @@
 #' @examples
 #' is_hex("#000000") #TRUE
 #' is_hex("#0000")   #FALSE
-is_hex <- function(color) {grepl("^\\#[0-6a-fA-F]{6}$", color)}
+is_hex <- function(color) {grepl("^\\#[0-9a-fA-F]{6}$", color)}
+
+#' Check if a string is a color
+#'
+#' @param color Color string to check
+#'
+#' @return A Boolean value.
+#' @noRd
+#'
+#' @examples
+#' check_color("red") #TRUE
+#' check_color("#249bc9") #TRUE
+#' check_color("#red") #FALSE
+check_color <- function(color) {
+    is_hex(color) | color %in% grDevices::colors()
+}
