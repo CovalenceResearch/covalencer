569b5d9531b57246fa9608370c203bad7554b26d
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-16 20:47:29 +0200

    Create test PSA data set

---
+++ tests/testthat/fixtures/make_test_data.R
@@ -0,0 +1,7 @@
+test_psa <- data.frame(
+    delta_qalys = rnorm(2000, 2, 2),
+    delta_costs = rnorm(2000, 10000, 1000),
+    country = rep(c("Country A", "Country B"), each = 1000)
+)
+
+saveRDS(test_psa, file = testthat::test_path("fixtures", "test_psa.rds"))
