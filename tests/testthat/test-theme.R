ad8c49736511de71e2c017158e72ee224ca566cb
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-16 20:02:26 +0200

    Add test for facets

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -3,5 +3,10 @@ test_that("ggplot theme works", {
                          ggplot2::aes(x = mpg, y = wt)) +
         ggplot2::geom_point() +
         theme_covalence()
+
+    p_facet <- p +
+        ggplot2::facet_wrap(ggplot2::vars(cyl))
+
     vdiffr::expect_doppelganger("themed scatterplot", p)
+    vdiffr::expect_doppelganger("faceted themed scatterplot", p_facet)
 })
