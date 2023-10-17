792a37f9901f26e36535b993a9a8f7f6faecaef7
Richard Pollock <88777235+RichardPollock@users.noreply.github.com>
2023-10-17 08:21:07 +0100

    Specify aesthetics required by stat_doublemean()

--- R/plot-psa.R
+++ R/plot-psa.R
@@ -213,7 +213,7 @@ plot_psa_scatter <- function(data,
 #' Calculate the mean of two variables
 #'
 #' @description
-#' Calculate the arithmetic mean of two variables, for subsequent use in a call to [`geom_point()`](https://ggplot2.tidyverse.org/reference/geom_point.html).
+#' Calculate the arithmetic mean of two variables, `x` and `y`, for subsequent use in a call to [`geom_point()`](https://ggplot2.tidyverse.org/reference/geom_point.html).
 #'
 #' @details
 #' The code for the `stat` and `layer` are heavily based on the [extending `ggplot2`](https://ggplot2.tidyverse.org/articles/extending-ggplot2.html) vignette.
