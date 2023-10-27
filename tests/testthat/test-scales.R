9ab588a0a961b4a77fa1f3f48d4ad58cf43b8db8
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-27 15:55:17 +0200

    Add test for sequential fill scale

--- tests/testthat/test-scales.R
+++ tests/testthat/test-scales.R
@@ -16,10 +16,17 @@ test_that("scales work", {
         ggplot2::geom_point(ggplot2::aes(color = price)) +
         scale_color_covalence_diverging(palette = "orange_purple")
 
+    p_sequential <- ggplot2::ggplot(ggplot2::diamonds,
+                                    ggplot2::aes(x = cut, y = color)) +
+        ggplot2::geom_tile(ggplot2::aes(fill = depth)) +
+        scale_fill_covalence_sequential(palette = "purples") +
+        ggplot2::theme(panel.grid = ggplot2::element_blank())
+
     vdiffr::expect_doppelganger("discrete color scale scatter",
                                 p_discrete_scatter)
     vdiffr::expect_doppelganger("discrete color scale histogram",
                                 p_discrete_histogram)
     vdiffr::expect_doppelganger("diverging color scale", p_diverging)
+    vdiffr::expect_doppelganger("sequential color scale", p_sequential)
 
 })
