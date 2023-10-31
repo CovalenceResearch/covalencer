676696835e0453db5921dd424084b066b11cc858
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-31 21:50:46 +0100

    Test main plot output

---
+++ tests/testthat/test-rob2.R
@@ -0,0 +1,15 @@
+test_that("rob2 summary plot works", {
+    data_rob2_summary <-
+        readRDS(test_path("fixtures", "test_rob2_summary.rds"))
+
+    # Full plot
+    p_full <- plot_rob2_summary(
+        data = data_rob2_summary,
+        domain = bias,
+        judgement = assess,
+        study_share = percentage
+    )
+
+    vdiffr::expect_doppelganger("Full RoB2 summary plot", p_full)
+
+})
