8a0ce558fc61e148ae97e440bf2cda51e4bfa746
Johannes Pöhlmann <poehlmannq@covalence-research.com>
2023-10-31 15:07:04 +0100

    Reformat code

--- R/plot_rob2.R
+++ R/plot_rob2.R
@@ -78,9 +78,11 @@ plot_rob2_summary <- function(data,
                                           check_sum$total == 100))])
 
     if (length(wrong_sum) > 0) {
-        cli::cli_alert_warning(cli::pluralize(
-            "Study shares for the {wrong_sum} domain{?s} don't sum to a sensible value."
-        ))
+        cli::cli_alert_warning(
+            cli::pluralize(
+                "Study shares for the {wrong_sum} domain{?s} don't sum to a sensible value."
+            )
+        )
     }
 
     ## Input types
